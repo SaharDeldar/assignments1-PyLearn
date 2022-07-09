c09d7a6f4d69bbaee4c0a49f0e489bfee6c24571
Sahar <100313500+SaharDeldar@users.noreply.github.com>
2022-07-09 16:24:28 +0430

    Add files via upload

---
+++ triangle.py
@@ -0,0 +1,8 @@
+a = int (input("Enter Your first side "))
+b = int (input("Enter Your second side "))
+c = int (input("Enter Your third side "))
+if a<b+c and b<a+c and c<a+b :
+    print("mitavan mosalas ra chap kard")
+else :
+    print("nemitavan mosalas ra chap kard")
+
